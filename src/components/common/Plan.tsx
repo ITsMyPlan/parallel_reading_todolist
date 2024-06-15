5da3703a827f7de4f1a0c9ffbcf66d62a2a0530f
drivehyrin <drivehyrin@gmail.com>
2024-06-15 22:27:36 +0900

    feat:plan 내부에 더보기 버튼 추가 DESC - as-is: plan 컨테이너를 클릭하면 detail 페이지로 이동 - to-be: plan 컨테이너 내부의 더보기 버튼을 클릭하면 detail 페이지로 이동

--- src/components/common/Plan.tsx
+++ src/components/common/Plan.tsx
@@ -21,13 +21,17 @@ function Plan(props: PlanProps) {
   }
 
   return (
-    <div className="border-8 border-solid border-yw-50/50 rounded-lg p-2 flex flex-col mt-5" onClick={goToDetail}>
-      <div>책 이름: {data.book_name}</div>
-      <div>저자: {data.author}</div>
-      <div>설명: {data.description}</div>
-      <div>시작일: {getFormattedDate(data.start_date)}</div>
-      <div>완료일: {getFormattedDate(data.end_date)}</div>
-    </div>
+    <>
+      <div className="border-8 border-solid border-yw-50/50 rounded-lg p-2 flex flex-col mt-5">
+        <button onClick={goToDetail}>more..</button>
+        <div>책 이름: {data.book_name}</div>
+        <div>저자: {data.author}</div>
+        <div>설명: {data.description}</div>
+        <div>시작일: {getFormattedDate(data.start_date)}</div>
+        <div>완료일: {getFormattedDate(data.end_date)}</div>
+        <button onClick={() => console.log('add daily goal for', data.id)}>+</button>
+      </div>
+    </>
   );
 }
 
