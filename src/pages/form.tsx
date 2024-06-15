4592c0d73db9fb1359f920279ee6639d1d965fec
drivehyrin <167775558+drivehyrin@users.noreply.github.com>
2024-06-15 14:50:04 +0900

    feat:새로운plan을 추가하는 initial form 생성 (#13)
    DESC
    - form 페이지는 새로운 plan을 만들거나 수정할 때 사용된다.
    - 우선 새로운 plan 생성할 때 사용하는 form을 생성하자

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,7 +1,48 @@
+import SubmitButton from '@components/common/Button';
+import InputContainer from '@/components/common/Container';
+import { useState } from 'react';
+
 function form() {
+  const initialFormData = {
+    book_name: '',
+    author: '',
+    description: '',
+    start_date: '',
+    end_date: ''
+  }
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value, name } } = event;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const onSubmitButtonClick = () => {
+    console.log('click!')
+  };
+
   return (
     <div>
-      Form for add or edit page
+      <InputContainer label="Book name">
+        <input type="text" name="book_name" placeholder='bookName' onChange={handleChange} value={formData.book_name} />
+      </InputContainer>
+      <InputContainer label="Author">
+        <input type="text" name="author" placeholder='authorName' onChange={handleChange} value={formData.author} />
+      </InputContainer>
+      <InputContainer label="Description">
+        <input type="text" name="description" placeholder='description' onChange={handleChange} value={formData.description} />
+      </InputContainer>
+      <InputContainer label="Start Date">
+        <input type="date" name="start_date" onChange={handleChange} value={formData.start_date} />
+      </InputContainer>
+      <InputContainer label="End Date">
+        <input type="date" name="end_date" onChange={handleChange} value={formData.end_date} />
+      </InputContainer>
+      <SubmitButton onClick={onSubmitButtonClick}>Submit</SubmitButton>
     </div>
   )
 }
