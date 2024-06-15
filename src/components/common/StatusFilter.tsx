bf2493eb10adcccf79716bb28c25bff0628bbf04
drivehyrin <drivehyrin@gmail.com>
2024-06-15 22:21:59 +0900

    feat:데일리 태스크 추가 버튼 추가 DESC - 각 plan에 대해 daily goal을 추가할 수 있다. - 우선 버튼만 추가하자

---
+++ src/components/common/StatusFilter.tsx
@@ -0,0 +1,14 @@
+import StatusBadge from '@components/common/StatusBadge'; 
+  
+function StatusFilter() {
+  const types = ['all', 'progress', 'todo', 'done']
+
+  return <div className="mt-10">
+    {
+      types.map((type, index) => {
+        return <StatusBadge key={index}>{type}</StatusBadge>
+      })
+    }
+  </div>
+}
+export default StatusFilter;
