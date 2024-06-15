1bbca2ebb23c7d5dd7efcb99fe86394354cd49d6
drivehyrin <167775558+drivehyrin@users.noreply.github.com>
2024-06-15 21:19:34 +0900

    feat:edit페이지 구현 (#19)
    DESC
    - detail 페이지에서 수정 버튼을 누르면 수정 인풋창이 보이도록 구현하자
    
    CHANGES
    - isEditing: 수정 중인 상태
    - updateEditingStatus: isEditing을 변경하는 메소드
    - editable: 수정 버튼이 보여도 되는 경우를 의미하는 boolean 값

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 
 interface ContainerProps {
   label: string,
-  children: React.ReactNode
+  children: React.ReactNode,
+  editable: boolean,
+  updateEditingStatus: (isEditing: boolean) => void,
+  isEditing: boolean
 }
 
 function Container(props: ContainerProps) {
-  const { children, label } = props;
+  const { children, label, editable, updateEditingStatus, isEditing } = props;
+
+  const onClickEditButton = () => {
+    if (isEditing) return;
+    updateEditingStatus(true);
+  }
 
   return (
-    <div className="border-8 border-solid border-yw-50/50 inset-blur rounded-lg p-3 flex flex-col m-5">
+    <div
+      className="border-8 border-solid border-yw-50/50 rounded-lg p-3 flex flex-col m-5"
+    >
+      {editable ? <button onClick={onClickEditButton} className="bg-yw-100">edit</button> : null}
       <span>{label}</span>
-      {children}
+      <span>{children}</span>
     </div>
   )
 }
