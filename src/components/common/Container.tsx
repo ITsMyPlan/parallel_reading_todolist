08d0a563ff288506207416ca6edba0aa13016def
drivehyrin <drivehyrin@gmail.com>
2024-06-17 19:10:56 +0900

    style:plan 상세 페이지 스타일 구현

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -2,26 +2,17 @@ import React from 'react';
 
 interface ContainerProps {
   label: string,
-  children: React.ReactNode,
-  editable: boolean,
-  updateEditingStatus: (isEditing: boolean) => void,
-  isEditing: boolean
+  children: React.ReactNode
 }
 
 function Container(props: ContainerProps) {
-  const { children, label, editable, updateEditingStatus, isEditing } = props;
-
-  const onClickEditButton = () => {
-    if (isEditing) return;
-    updateEditingStatus(true);
-  }
+  const { children, label } = props;
 
   return (
     <div
       className="border-8 border-solid border-yw-50/50 rounded-lg p-3 flex flex-col m-5"
     >
-      {editable ? <button onClick={onClickEditButton} className="bg-yw-100">edit</button> : null}
-      <span>{label}</span>
+      <span className="text-gr-70 font-semibold text-lg">{label}</span>
       <span>{children}</span>
     </div>
   )
