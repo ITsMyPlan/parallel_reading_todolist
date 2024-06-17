44615c2247c98f7e79847d4c3e37793c9b86fe81
drivehyrin <drivehyrin@gmail.com>
2024-06-17 20:37:43 +0900

    feature:index 화면 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import StartButton from '@/components/common/Button';
+import logoImage from '@/assets/pr_logo.png';
+import { useNavigate } from "react-router-dom";
+
 function index() {
+  const navigate = useNavigate();
+  const onStartButtonClick = () => {
+    navigate('/home');
+  }
+
   return (
     <div>
-      Intro Page
+      <img className="mt-40 mb-10" src={logoImage}></img>
+      <div className='flex flex-col items-center justify-center mb-10'>
+        <div className="text-4xl text-gray-900">Parallel Reading</div>
+        <div className='text-4xl text-gray-900'>Todo list</div>
+      </div>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='text-xl text-gray-700'>매일매일 조금씩 여러 권 읽기</div>
+      </div>
+      <StartButton path="" onButtonClick={onStartButtonClick}>Start Now!</StartButton>
     </div>
   )
 }
